ffffb2a2fbc219c8e69d1c0754aacafcdb8007f4
leelachouhan23 <leelachouhan23@navgurukul.org>
2026-03-06 18:55:34 +0530

    submitting assignment

---
+++ second.js
@@ -0,0 +1,92 @@
+
+// 1.Write a JavaScript program to take the length and breadth of a rectangle and calculate the area of the rectangle.
+
+// let length = 5;
+// let breadth = 7;
+
+// let areaOfRactangle = length * breadth;
+// console.log(`area of ractangular is ${areaOfRactangle}cm`)
+
+// 2.Write a JavaScript program to take a number and print the square of the number.
+// let a = 4;
+// console.log(a**2)
+
+
+
+
+
+//3.Write a JavaScript program to take two numbers a and b and print only the integer part when a is divided by b.
+
+// let a = 7;
+// let b = 2;
+// console.log(Math.floor(a/b))
+
+//4. Write a JavaScript program to take a number and print the last digit of the number.
+
+// let a = 245;
+// console.log(a % 10);
+
+
+
+// 5.Write a JavaScript program to take a number and print the cube of that number.
+
+// let a = 2;
+// console.log(a**3)
+
+
+
+//6. Write a JavaScript program to take a three-digit number and print the reverse of that number.
+//  Example: 235 → 532
+
+// let a = 235;
+// let reverse = 0;
+
+// while(a > 0){
+
+//    let digit = a % 10;
+//    reverse = reverse * 10 + digit;
+//    a = Math.floor(a/10);
+// }
+// console.log(reverse)
+
+
+//7. Write a JavaScript program to take a number and print the first digit of the number.
+//  Example: 145 → 1, 789 → 7
+
+// let a = 145;
+// console.log(Math.floor(a/100))
+
+
+
+//8. Write a JavaScript program to take a number and print the sum of the first and last digit.
+//  Example: 10 → 1 + 0 = 1, 452 → 4 + 2 = 6
+
+// let a = 4455;
+// let first = Math.floor(a / 1000);
+// let last = a % 10;
+// console.log(first + last);
+
+
+// 9.Write a JavaScript program to take the salary of five employees and print the average salary of all employees.
+// let fE= 60;
+// let sE = 80;
+// let tE= 90;
+// let forthE= 70;
+// let fifthE= 68;
+
+// let Sum = fE + sE + tE + forthE + fifthE;
+
+// let average = Math.floor(Sum / 5);
+// console.log(`The avrage of 5 employees salary is ${average}`);
+
+//10.Write a JavaScript program to take two numbers a and b and swap their values.
+//  Example: a = 10, b = 5 → a = 5, b = 10
+
+// let a = 10;
+// let b = 5;
+
+
+// a = a + b;
+// b = a - b;
+// a =  a - b;
+// console.log(`now a = ${a}, b = ${b}`)
